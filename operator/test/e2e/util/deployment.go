f0c8f287661a6217a6af9034e87319c33d3ceab2
chuyanguo <chuyanguo@163.com>
2021-09-01 10:18:11 +0800

    1

--- operator/test/e2e/util/deployment.go
+++
@@ -1,5 +0,0 @@
-package util
-
-func waitForDeploymentPodReady() error {
-	return nil
-}
--- quick-startup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "mysql mysql startup"
-kubectl create -f ./deploy/mysql/mysql-local.yaml
-
-
-echo "nacos quick startup"
-kubectl create -f ./deploy/nacos/nacos-quick-start.yaml
